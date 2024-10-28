ae85fc9df3e713541a5492ca33edc6ef1fda84c9
James Vaughan <james@jamesbvaughan.com>
2024-10-27 18:38:41 -0700

    More updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["hugo_stats.json", "assets/*.svg"],
-  theme: {
-    extend: {
-      animation: {
-        "spin-33rpm": "spin 1.8s linear infinite",
-      },
-    },
-  },
-  plugins: [],
+  content: ["hugo_stats.json"],
 };
